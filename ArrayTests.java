7b9c9a6881b06b7ae402baea6305397652e20a22
noemyvalencia <130100414+noemyvalencia@users.noreply.github.com>
2023-04-20 12:58:18 -0700

    ReversedInPlace and reversed fixed.

--- ArrayTests.java
+++ ArrayTests.java
@@ -15,4 +15,15 @@ public class ArrayTests {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
   }
+  @Test
+  public void testReverseInPlace2(){
+    int[] in = {3, 8, 45, 79};
+    ArrayExamples.reverseInPlace(in);
+    assertArrayEquals(new int[]{79, 45, 8, 3}, in);
+  }
+  @Test
+  public void testReversed2(){
+    int[] in2 = {3, 0, 100, 25, 38};
+    assertArrayEquals(new int[]{38, 25, 100, 0, 3}, ArrayExamples.reversed(in2));
+  }
 }
